34761b7c1a2f5f472f31c5c44229b84bf133ee44
Adilson Neto <almeidneto@gmail.com>
2019-05-10 00:05:47 -0300

    Use markov chain to generate names

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-
+mod markov;
 mod scraper;
 
 fn main() {
+    let mut chain = markov::MarkovChain::new(3);
+
     for name in scraper::find_names_in_page() {
+        chain.feed_str(name);
+    }
+
+    for _ in 0..15 {
+        let name = chain.generate_str(7);
+
         println!("{}", name);
     }
 }
