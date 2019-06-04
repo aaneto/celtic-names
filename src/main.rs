0daef9f587355a35534d3817d13cbb50d2abec7e
Adilson Neto <almeidneto@gmail.com>
2019-06-03 22:29:44 -0300

    Bump version 0.1.3

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use clap::{Arg, App};
 
 fn main() {
     let matches = App::new("Celtic Markov Names")
-        .version("0.1.2")
+        .version("0.1.3")
         .author("Adilson Neto <almeidneto@gmail.com>")
         .about("Generate a random celtic name using markov chains")
         .arg(
